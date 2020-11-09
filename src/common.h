666a7e8af4f21737ff6596937346f23ffb8b2e4b
Limem Slim <72703220+slimem@users.noreply.github.com>
2020-11-09 01:13:22 +0100

    Create common.h

---
+++ src/common.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/gtc/matrix_transform.hpp>
